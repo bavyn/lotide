9f866e6790027bc429efe9772ee25748d820d171
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-01 19:06:01 -0700

    created assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertation Passed: " + actual + " === " + expected);
+  } else
+    console.log("❌ Assertation Failed: " + actual + " !== " + expected);
+};
+
+// test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Karma", "Charlie");
+assertEqual(100, 100);
+assertEqual("kiwi", "kiwi");
+assertEqual(123, 987);
