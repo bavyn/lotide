e1244cc7b087f117a494338ba9420a6e91396e1d
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-02 20:58:27 -0700

    created eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+// function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertation Passed: ${actual}  === ${expected}`);
+  } else
+    console.log(`❌ Assertation Failed: ${actual}  !== ${expected}`);
+};
+
+let eqArrays = function(array1,array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
