ab4959a9b233b0cce145f88f1eed1ecd65f6ae2e
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-08 15:36:32 -0700

    refactored assertEqual and head with moduleexports and created a test folder to hold the test files

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,3 @@
-// function implementation
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertation Passed: ${actual}  === ${expected}`);
@@ -6,10 +5,4 @@ const assertEqual = function(actual, expected) {
     console.log(`❌ Assertation Failed: ${actual}  !== ${expected}`);
 };
 
-// test code
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Karma", "Charlie");
-assertEqual(100, 100);
-assertEqual("kiwi", "kiwi");
-assertEqual(123, 987);
+module.exports = assertEqual;
