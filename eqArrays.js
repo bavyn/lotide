c1f594a7422fb4a1b500f5c748488adfbdf68943
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-08 16:26:21 -0700

    refactored eqArrays to use moduleexports and added eqArraysTest file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-// function implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertation Passed: ${actual}  === ${expected}`);
-  } else
-    console.log(`❌ Assertation Failed: ${actual}  !== ${expected}`);
-};
-
 let eqArrays = function(list1,list2) {
   for (let i = 0; i < list1.length; i++) {
     if (list1[i] !== list2[i]) {
@@ -15,9 +7,4 @@ let eqArrays = function(list1,list2) {
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
+module.exports = eqArrays;
