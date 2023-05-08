311085c09de5d393cf7722c59bd8318455684485
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-08 15:44:16 -0700

    refactored tail, this is the same as the previous commit but i forgot to save

--- tail.js
+++ tail.js
@@ -1,24 +1,5 @@
-// assertEqual function implementation
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertation Passed: ${actual}  === ${expected}`);
-  } else
-    console.log(`❌ Assertation Failed: ${actual}  !== ${expected}`);
-};
-
-// tail function implementation
 let tail = function(myArray) {
   return myArray.slice(1);
 };
 
 module.exports = tail;
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
